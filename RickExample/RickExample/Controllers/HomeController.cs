0fe1ad52ac3620eb90b003cf427dacb5eec8645c
Scott Dockendorf <scott@dockendorf.org>
2018-02-27 11:43:41 -0600

    Initial Commit

---
+++ RickExample/RickExample/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using RickExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RickExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var vm = new FilesViewModel();
+            var files = System.IO.Directory.GetFiles(@"C:\code\me\RickExample\RickExample\fonts");
+            vm.FileNames = files;
+            return View(vm);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ RickExample/RickExample/Models/FilesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RickExample.Models
+{
+    public class FilesViewModel
+    {
+        public IEnumerable<string> FileNames { get; set; }
+    }
+}
